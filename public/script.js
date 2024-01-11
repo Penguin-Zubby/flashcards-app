a7deb835509e41720b020291b429d17a0e8278ed
Penguin-Zubby <140756585+Penguin-Zubby@users.noreply.github.com>
2024-01-11 22:39:07 +0000

    Added icon image fixed all bugs

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
+//this var reterives data from the local storage
 var contentArray = localStorage.getItem('items')? 
 JSON.parse(localStorage.getItem('items')) : [];
-
+//reteriving all the alliments created in html file and assigning them functions
 document.getElementById("save_card").addEventListener("click", () => {addFlashcard();});
 
 document.getElementById("delete_cards").addEventListener("click", () => 
@@ -15,7 +16,7 @@ document.getElementById("show_card_box").addEventListener("click", () => {
 document.getElementById("close_card_box").addEventListener("click", () => {
     document.getElementById("create_card").style.display = "none";});
 
-
+//Creating a function that will create element in the dom from div to headings.
 flashcardMaker = (text, delThisIndex) => {
     const flashcard = document.createElement('div');
     const question = document.createElement('h2');
@@ -23,11 +24,11 @@ flashcardMaker = (text, delThisIndex) => {
     const del = document.createElement('i');
 
     flashcard.className = 'flashcard';
-
+    //setting attributes for the flashcard labels
     question.setAttribute('style', "border-top: 1px solid red; padding:15px; margin-top: 30px");
     question.textContent = text.my_question;
 
-    answer.setAttribute('style', "text-align:center; display:none; color:green");
+    answer.setAttribute('style', "text-align:center; display:flex; flex-wrap:wrap; justify-content:flex-start; color:green");
     answer.textContent = text.my_answer;
 
     del.className = "fas fa-minus";
@@ -37,15 +38,16 @@ flashcardMaker = (text, delThisIndex) => {
         window.location.reload();
     })
 
-    flashcard.appendchild(question);
+    flashcard.appendChild(question);
     flashcard.appendChild(answer);
     flashcard.appendChild(del);
 
+    //this function is a loop that will keep adding the empty flashcard to the screen
     flashcard.addEventListener('click', () => {
         if(answer.style.display === 'none')
-          answer.style.display = 'none';
-        else
           answer.style.display = 'block';
+        else
+          answer.style.display = 'none';
     });
 
     document.querySelector("#flashcards").appendChild(flashcard);
@@ -59,7 +61,7 @@ addFlashcard= () => {
 
     var flashcard_info = {
         'my_question': question.value,
-        'my_answer': answer.value
+        'my_answer': answer.value,
     }
     contentArray.push(flashcard_info);
     localStorage.setItem('items', JSON.stringify(contentArray));
@@ -67,4 +69,5 @@ addFlashcard= () => {
     question.value = "";
     answer.value = "";
 }
+
 
